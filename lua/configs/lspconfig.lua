f1bbc577cf938a7974418c398e5fbca3f2e45ef3
Lysandder <eketcum61@gmail.com>
2025-02-14 22:30:20 +0500

    Changed indentation size to 2 in neovim itself and created ~/.config/pep8 file to configure autopep8.

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -12,17 +12,43 @@ local servers = {
   "ts_ls",
 }
 
-local nvlsp = require "nvchad.configs.lspconfig"
+local nvlsp = require("nvchad.configs.lspconfig")
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
+  lspconfig[lsp].setup({
     on_attach = nvlsp.on_attach,
     on_init = nvlsp.on_init,
     capabilities = nvlsp.capabilities,
-  }
+  })
 end
 
+-- Ensure Neovim enforces 2-space indentation for all buffers
+-- With filetypes specified
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = {
+    "html",
+    "css",
+    "python",
+    "typescript",
+    "javascript",
+    "lua",
+    "cpp",
+    "c",
+  },
+
+  callback = function()
+    vim.bo.tabstop = 2
+    vim.bo.shiftwidth = 2
+    vim.bo.softtabstop = 2
+    vim.bo.expandtab = true -- Use spaces instead of tabs
+  end,
+})
+-- To change indentation size in autopep8 I had to create a file ~/.config/pep8 and write in it:
+-- [pep8]
+-- indent-size = 2
+
+-- Typescript example
 -- configuring single server, example: typescript
 -- lspconfig.ts_ls.setup {
 --   on_attach = nvlsp.on_attach,
