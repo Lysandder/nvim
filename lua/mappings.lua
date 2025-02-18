bec0e82c1b50c78915235ba04923693ffd052871
Lysandder <eketcum61@gmail.com>
2025-02-18 11:50:54 +0500

    Added shortcuts to enter and quit terminal mode in nvim.

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,6 +5,8 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+-- map("i", "jk", "<ESC>")
+map("t", "<Esc>", [[<C-\><C-n>]], { noremap = true, silent = true })
+map("n", "<leader>t", ":startinsert!<CR>", { noremap = true, silent = true })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
