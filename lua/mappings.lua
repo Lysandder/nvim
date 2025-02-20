1528b782d5902718875590cf8cb0237cd306d687
Lysandder <eketcum61@gmail.com>
2025-02-20 13:00:46 +0500

    Added a shortcut (fj) to quit the insert mode.

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,7 +5,7 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
--- map("i", "jk", "<ESC>")
+map("i", "fj", "<ESC>")
 map("t", "<Esc>", [[<C-\><C-n>]], { noremap = true, silent = true })
 map("n", "<leader>t", ":startinsert!<CR>", { noremap = true, silent = true })
 
